b1eb2c5ca5841d2a2d9a03d974ea7b41fe1b1514
Joao Neves <joao@daily.co>
2022-11-08 16:52:18 +0100

    Add aspectRatio to MediaTrackConstraints

--- crates/web-sys/src/features/gen_MediaTrackConstraints.rs
+++ crates/web-sys/src/features/gen_MediaTrackConstraints.rs
@@ -19,6 +19,23 @@ impl MediaTrackConstraints {
         let mut ret: Self = ::wasm_bindgen::JsCast::unchecked_into(::js_sys::Object::new());
         ret
     }
+    #[doc = "Change the `aspectRatio` field of this object."]
+    #[doc = ""]
+    #[doc = "*This API requires the following crate features to be activated: `MediaTrackConstraints`*"]
+    pub fn aspect_ratio(&mut self, val: &::wasm_bindgen::JsValue) -> &mut Self {
+        use wasm_bindgen::JsValue;
+        let r = ::js_sys::Reflect::set(
+            self.as_ref(),
+            &JsValue::from("aspectRatio"),
+            &JsValue::from(val),
+        );
+        debug_assert!(
+            r.is_ok(),
+            "setting properties should never fail on our dictionary objects"
+        );
+        let _ = r;
+        self
+    }
     #[doc = "Change the `autoGainControl` field of this object."]
     #[doc = ""]
     #[doc = "*This API requires the following crate features to be activated: `MediaTrackConstraints`*"]
